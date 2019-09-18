d4649b6b47527ce1eae2dff908558a5b1de875a3
Max <maximumpwnguin@gmail.com>
2019-09-18 00:17:27 -0700

    Fill mode tab

---
+++ src/main/kotlin/io/plenglin/dormled/FillModeView.kt
@@ -0,0 +1,45 @@
+package io.plenglin.dormled
+
+import javafx.geometry.Orientation
+import javafx.scene.Parent
+import javafx.scene.control.Slider
+import tornadofx.*
+import java.util.logging.Logger
+
+class FillModeView : View() {
+    private val logger = Logger.getLogger(javaClass.canonicalName)
+    private val bluetoothController: BluetoothController by inject()
+
+    lateinit var r: Slider
+    lateinit var g: Slider
+    lateinit var b: Slider
+
+    override val root: Parent = gridpane {
+        row {
+            label("Red")
+            r = slider(0, 1, 0) {
+                valueProperty().onChange { doFill() }
+            }
+        }
+        row {
+            label("Green")
+            g = slider(0, 1, 0) {
+                valueProperty().onChange { doFill() }
+            }
+        }
+        row {
+            label("Blue")
+            b = slider(0, 1, 0) {
+                valueProperty().onChange { doFill() }
+            }
+        }
+    }
+
+    private fun doFill() {
+        val r = (r.value * 255).toInt()
+        val g = (g.value * 255).toInt()
+        val b = (b.value * 255).toInt()
+        bluetoothController.connection!!.fill(r, g, b)
+    }
+
+}
--- src/main/kotlin/io/plenglin/dormled/MainWindowView.kt
+++ src/main/kotlin/io/plenglin/dormled/MainWindowView.kt
@@ -1,5 +1,6 @@
 package io.plenglin.dormled
 
+import javafx.application.Platform
 import javafx.scene.Parent
 import tornadofx.View
 import tornadofx.borderpane
@@ -8,5 +9,9 @@ class MainWindowView : View() {
     override val root: Parent = borderpane {
         top<BluetoothSelectionView>()
         center<LEDControlView>()
+    }
+
+    override fun onDelete() {
+        Platform.exit()
     }
 }
